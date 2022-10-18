8ca81de8f41a309ff8023f694cff37af6eb0163f
kasiwa-mesi <italynataly@outlook.jp>
2022-10-18 10:13:14 +0900

    first commit

--- SampleEmailLogin/Login/LoginViewController.swift
+++ SampleEmailLogin/Login/LoginViewController.swift
@@ -1,19 +1,15 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  SampleEmailLogin
 //
 //  Created by kasiwa on 2022/10/18.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+final class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
