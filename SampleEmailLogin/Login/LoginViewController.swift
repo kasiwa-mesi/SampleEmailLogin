2b254031835781d32a987dea11cf0e94578f86e2
kasiwa-mesi <italynataly@outlook.jp>
2022-10-20 09:51:18 +0900

    :hammer: TextField&Buttonの値をViewControllerに渡す

--- SampleEmailLogin/Login/LoginViewController.swift
+++ SampleEmailLogin/Login/LoginViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton! {
+        didSet {
+            loginButton.addTarget(self, action: #selector(tapLoginButton), for: .touchUpInside)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+private extension LoginViewController {
+    @objc func tapLoginButton() {
+        //ログイン処理を行う
+        print("ログイン")
     }
 }
--- SampleEmailLogin/Register/RegisterViewController.swift
+++ SampleEmailLogin/Register/RegisterViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 final class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton! {
+        didSet {
+            registerButton.addTarget(self, action: #selector(tapRegisterButton), for: .touchUpInside)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,3 +31,11 @@ final class RegisterViewController: UIViewController {
         return vc
     }
 }
+
+private extension RegisterViewController {
+    @objc func tapRegisterButton() {
+        //会員登録処理を行う
+        print("会員登録する")
+    }
+}
+
