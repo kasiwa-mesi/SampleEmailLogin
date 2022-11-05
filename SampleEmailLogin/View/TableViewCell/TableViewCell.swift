79ab387cb2f52754342d46a28a6dc1596792a5ca
kasiwa-mesi <italynataly@outlook.jp>
2022-11-05 22:01:39 +0900

    :sparkles: TableViewのセルとしてxibファイルを作成

---
+++ SampleEmailLogin/View/TableViewCell/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  SampleEmailLogin
+//
+//  Created by kasiwa on 2022/11/05.
+//
+
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var memoImage: UIImageView!
+    @IBOutlet weak var memoTextLabel: UILabel!
+    @IBOutlet weak var memoCreatedAt: UILabel!
+}
