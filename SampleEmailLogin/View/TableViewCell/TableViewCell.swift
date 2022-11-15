3f59570338c5404007a2a9a1c703db0ad8714612
kasiwa-mesi <italynataly@outlook.jp>
2022-11-15 13:09:37 +0900

    :sparkles: ホーム画面にメモの画像を表示できるように実装

--- SampleEmailLogin/View/TableViewCell/TableViewCell.swift
+++ SampleEmailLogin/View/TableViewCell/TableViewCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwiftDate
+import Kingfisher
 
 class TableViewCell: UITableViewCell {
     @IBOutlet weak var memoImage: UIImageView!
@@ -21,9 +22,17 @@ class TableViewCell: UITableViewCell {
     }
     
     func configure(memo: MemoModel) {
-        if let imageUrlStr = memo.imageURL   {
+        print(memo)
+        if let url = URL(string: memo.imageURLStr) {
+            print("URLから画像を読み込む")
+            memoImage.kf.indicatorType = .activity
+            memoImage.kf.setImage(with: url)
+            print(memoImage)
+        } else {
+            print("画像がないので非表示")
+            memoImage.isHidden = true
         }
-
+        
         memoTextLabel.text = memo.text
         // memo.createdAtのData型を文字列にする
         let createdAt = "\(memo.createdAt.year)年\(memo.createdAt.month)月\(memo.createdAt.day)日"
